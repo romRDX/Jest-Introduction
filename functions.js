42b704f16aa94f118e8c1c9e787cb64577e12df5
Rom <romullo_sas@outlook.com>
2019-08-28 17:50:17 -0300

    Arquivo functions.js pronto para atividades

--- functions.js
+++ functions.js
@@ -1,4 +1,3 @@
-const axios = require('axios');
 
 const functions = {
     add: (num1, num2) => num1 + num2, // +1
@@ -8,11 +7,7 @@ const functions = {
         const user = { firstName: 'Brad' }
         user['lastName'] = "Traversy";
         return user;
-    },
-    fetchUser: ()=> axios.get('https://jsonplaceholder.typicode.com/users/1')
-    .then(res => res.data)
-    .catch(err = 'error'),
-    reverseString: (string)=> string.split('').reverse().join('')
+    }
 }
 
 module.exports = functions;
